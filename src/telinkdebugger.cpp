ce49c65a0ba3acd61e86cd53032dbcdd249d59a8
Novan24 <ignitinechannel24@gmail.com>
2026-07-31 09:09:29 +0700

    temporary debug

--- src/telinkdebugger.cpp
+++ src/telinkdebugger.cpp
@@ -421,7 +421,7 @@ static uint8_t read_hex_byte()
 char buffer[3];
 buffer[0] = getchar();
 buffer[1] = getchar();
-buffer[2] = 0
+buffer[2] = 0;
 
 //temporary debug
 printf("# hex chars = '%c%c'\n", buffer[0], buffer[1]);
